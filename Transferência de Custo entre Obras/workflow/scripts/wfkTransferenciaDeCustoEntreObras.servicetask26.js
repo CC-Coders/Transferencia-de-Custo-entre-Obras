29d7f9765050503ae84edec75b07b8b02c16caf8
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2025-07-09 10:09:27 -0300

    Incluí evento para alterar o STATUS da Transferência para Finalizado

--- Transferência de Custo entre Obras/workflow/scripts/wfkTransferenciaDeCustoEntreObras.servicetask26.js
+++ Transferência de Custo entre Obras/workflow/scripts/wfkTransferenciaDeCustoEntreObras.servicetask26.js
@@ -1,2 +1,14 @@
 function servicetask26(attempt, message) {
+    atualizaStatusTransferencia(2);
+}
+
+
+function atualizaStatusTransferencia(STATUS){
+    var id = hAPI.getCardValue("ID_TRANSFERENCIAS_DE_CUSTO");
+
+    var query = "UPDATE TRANSFERENCIAS_DE_CUSTO SET STATUS = ? WHERE ID = ?";
+    executaUpdate(query, [
+        {type:"int", value:STATUS},
+        {type:"int", value:id},
+    ]);
 }

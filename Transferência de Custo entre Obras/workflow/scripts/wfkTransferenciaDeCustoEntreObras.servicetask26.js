7387530cb1b00bb5ccce72884dd71ee59753a852
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2025-07-16 10:29:37 -0300

    Incluí a Integração para Gerar os Movimentos 1.1.93 na Obra Origem e Destino

--- Transferência de Custo entre Obras/workflow/scripts/wfkTransferenciaDeCustoEntreObras.servicetask26.js
+++ Transferência de Custo entre Obras/workflow/scripts/wfkTransferenciaDeCustoEntreObras.servicetask26.js
@@ -1,8 +1,199 @@
 function servicetask26(attempt, message) {
-    atualizaStatusTransferencia(2);
+    var CODCOLIGADA_ORIGEM = hAPI.getCardValue("ccustoObraOrigem").split(" - ")[0];
+    var CODCCUSTO_ORIGEM = hAPI.getCardValue("ccustoObraOrigem").split(" - ")[1];
+    var NOMECCUSTO_ORIGEM = hAPI.getCardValue("ccustoObraOrigem").split(" - ")[2];
+    
+    var CODCOLIGADA_DESTINO = hAPI.getCardValue("ccustoObraDestino").split(" - ")[0];
+    var CODCCUSTO_DESTINO = hAPI.getCardValue("ccustoObraDestino").split(" - ")[1];
+    var NOMECCUSTO_DESTINO = hAPI.getCardValue("ccustoObraDestino").split(" - ")[2];
+
+    var PRODUTOS = getProdutos(CODCOLIGADA_ORIGEM, CODCOLIGADA_DESTINO);
+
+
+    // Lanca ajuste para Obra Origem
+    // Se for TRANSFERE_CUSTO, então a Obra Origem, recebe o PRODUTO AUMENTA_RESULTADO, pois está enviando o CUSTO
+    // Porém, se for TRANSFERE_RECEITA, então a Obra Origem, recebe o PRODUTO DIMINUI_RESULTADO, pois está enviando a RECEITA
+    var PRODUTO_ORIGEM = hAPI.getCardValue("TRANSFERE_CUSTO") == "true" ? PRODUTOS.AUMENTA_RESULTADO : PRODUTOS.DIMINUI_RESULTADO;
+    var idmov = geraMovimentos(CODCOLIGADA_ORIGEM, CODCCUSTO_ORIGEM, NOMECCUSTO_ORIGEM, PRODUTO_ORIGEM);
+    hAPI.setCardValue("IDMOV_ORIGEM", idmov);
+    
+    
+    // Lanca ajuste para Obra Destino
+    // Se for TRANSFERE_CUSTO, então a Obra Destino, recebe o PRODUTO DIMINUI_RESULTADO, pois está recebendo CUSTO
+    // Porém, se for TRANSFERE_RECEITA, então a Obra Destino, recebe o PRODUTO AUMENTA_RESULTADO, pois está recebendo a RECEITA
+    var PRODUTO_DESTINO = hAPI.getCardValue("TRANSFERE_CUSTO") == "true" ? PRODUTOS.DIMINUI_RESULTADO : PRODUTOS.AUMENTA_RESULTADO;
+    var idmov = geraMovimentos(CODCOLIGADA_DESTINO, CODCCUSTO_DESTINO, NOMECCUSTO_DESTINO, PRODUTO_DESTINO);
+    hAPI.setCardValue("IDMOV_DESTINO", idmov);
+
+
+    atualizaStatusTransferencia(STATUS_TRANSFENCIA.APROVADO);
 }
 
+function getProdutos(CODCOLIGADA_ORIGEM, CODCOLIGADA_DESTINO) {
+    if (CODCOLIGADA_ORIGEM == CODCOLIGADA_DESTINO) {
+        // Se transferencia entre Centro de Custo
+        var PRODUTOS = {
+            AUMENTA_RESULTADO: {
+                IDPRD: "1514",
+                CODIGOPRD: "11.006.00110",
+                DESCPRD: "Ajustes gerenciais (+)",
+            },
+            DIMINUI_RESULTADO: {
+                IDPRD: "1515",
+                CODIGOPRD: "11.006.00111",
+                DESCPRD: "Ajustes gerenciais (-)",
+            }
+        }
+        return PRODUTOS;
+    } else {
+        // Se Transferencia entre Coligadas
+        var PRODUTOS = {
+            AUMENTA_RESULTADO: {
+                IDPRD: "11.006.00158",
+                CODIGOPRD: "11.006.00158",
+                DESCPRD: "Ajuste entre coligadas (+)",
+            },
+            DIMINUI_RESULTADO: {
+                IDPRD: "11.006.00159",
+                CODIGOPRD: "11.006.00159",
+                DESCPRD: "Ajuste entre coligadas (-)",
+            }
+        }
+        return PRODUTOS;
+    }
+}
 
+function geraMovimentos(CODCOLIGADA, CODCCUSTO, NOMECCUSTO, PRODUTO){
+    var CODLOC = buscaLocalEstoquePorNome(CODCOLIGADA, 1, NOMECCUSTO);
+    var xml = montaXML(CODCOLIGADA, CODLOC, CODCOLIGADA, "00001", PRODUTO, CODCCUSTO);
+
+    var idmov = ImportaMovimento(CODCOLIGADA, xml);
+    log.info("Movimentos inseridos: " + idmov);
+    return idmov;
+
+}
+
+function montaXML(CODCOLIGADA, CODLOC, CODCOLCFO, CODCFO, PRODUTO, CODCCUSTO) {
+    var URLFluig = getServerURL() + "portal/p/1/pageworkflowview?app_ecm_workflowview_detailsProcessInstanceID=" + getValue("WKNumProces")  ;
+    
+    var xml = "";
+    xml += "<MovMovimento>";
+    xml += "    <TMOV>";
+    xml += "        <CODCOLIGADA>" + CODCOLIGADA + "</CODCOLIGADA>";
+    xml += "        <IDMOV>-1</IDMOV>";
+    xml += "        <NUMEROMOV>-1</NUMEROMOV> ";
+    xml += "        <CODFILIAL>1</CODFILIAL>";
+    xml += "        <CODLOC>" + CODLOC + "</CODLOC>";
+    xml += "        <CODTMV>1.1.93</CODTMV>";
+    xml += "        <TIPO>A</TIPO>";
+    xml += "        <STATUS>A</STATUS>";
+    xml += "        <DATAEMISSAO>" + getDateNow() + "</DATAEMISSAO>";
+    xml += "        <DATASAIDA>" + getDateNow() + "</DATASAIDA>";
+    xml += "        <CODMOEVALORLIQUIDO>R$</CODMOEVALORLIQUIDO>";
+    xml += "        <INTEGRAAPLICACAO>T</INTEGRAAPLICACAO>";
+    xml += "        <CODCOLIGADA1>" + CODCOLIGADA + "</CODCOLIGADA1>";
+    xml += "        <CODCFO>" + CODCFO + "</CODCFO>";
+    xml += "        <CODCFOAUX>" + CODCFO + "</CODCFOAUX>";
+    xml += "        <CODCOLCFO>" + CODCOLCFO + "</CODCOLCFO>";
+    xml += "        <CODCPG>001</CODCPG>";
+    xml += "    </TMOV>";
+
+
+    var IDPRD = PRODUTO.IDPRD;
+    var CODIGOPRD = PRODUTO.CODIGOPRD;
+    var DESCPRODUTO = PRODUTO.DESCPRD;
+
+    // var DESCITEM = item.DescricaoItem;
+    var QUANTIDADE = 1;
+    var VALORUNIT = moneyToFloat(hAPI.getCardValue("valorObraDestino")).toString().replace(".", ",");
+    var CODUND = "UN";
+
+    xml += "<TITMMOV>";
+    xml += "    <CODCOLIGADA>" + CODCOLIGADA + "</CODCOLIGADA>";
+    xml += "    <IDMOV>-1</IDMOV>";
+    xml += "    <CODLOC>" + CODLOC + "</CODLOC>";
+    xml += "    <NSEQITMMOV>1</NSEQITMMOV>";
+    xml += "    <NUMEROSEQUENCIAL>1</NUMEROSEQUENCIAL>";
+    xml += "    <IDPRD>" + IDPRD + "</IDPRD>";
+    xml += "    <CODIGOPRD>" + CODIGOPRD + "</CODIGOPRD>";
+    xml += "    <NOMEFANTASIA>" + DESCPRODUTO + "</NOMEFANTASIA>";
+    xml += "    <QUANTIDADE>" + QUANTIDADE + "</QUANTIDADE>";
+    xml += "    <PRECOUNITARIO>" + VALORUNIT + "</PRECOUNITARIO>";
+    xml += "    <CODUND>" + CODUND + "</CODUND>";
+    xml += "    <VALORUNITARIO>" + VALORUNIT + "</VALORUNITARIO>";
+    // xml += "    <HISTORICOCURTO>" + DESCITEM + "</HISTORICOCURTO>";
+    xml += "    <INTEGRAAPLICACAO>T</INTEGRAAPLICACAO>";
+    xml += "    <IDMOVHST>-1</IDMOVHST>";
+    xml += "    <CODCOLIGADA1>" + CODCOLIGADA + "</CODCOLIGADA1>";
+    xml += "    <NSEQITMMOV1>1</NSEQITMMOV1>";
+    xml += "</TITMMOV>"
+
+
+    xml += "<TITMMOVRATCCU>";
+    xml += "    <CODCOLIGADA>" + CODCOLIGADA + "</CODCOLIGADA>";
+    xml += "    <IDMOV>-1</IDMOV>";
+    xml += "    <NSEQITMMOV>1</NSEQITMMOV>";
+    xml += "    <CODCCUSTO>" + CODCCUSTO + "</CODCCUSTO>";
+    xml += "    <PERCENTUAL>100</PERCENTUAL>";
+    xml += "    <IDMOVRATCCU>-1</IDMOVRATCCU>";
+    xml += "</TITMMOVRATCCU>";
+
+
+    xml += "<TITMMOVRATDEP>";
+    xml += "    <CODCOLIGADA>" + CODCOLIGADA + "</CODCOLIGADA>";
+    xml += "    <IDMOV>-1</IDMOV>";
+    xml += "    <NSEQITMMOV>1</NSEQITMMOV>";
+    xml += "    <CODFILIAL>1</CODFILIAL>";
+    xml += "    <CODDEPARTAMENTO>1.3.01</CODDEPARTAMENTO>";
+    xml += "    <PERCENTUAL>100</PERCENTUAL>";
+    xml += "</TITMMOVRATDEP>";
+
+
+    xml += "<TMOVCOMPL>";
+    xml += "    <CODCOLIGADA>" + CODCOLIGADA + "</CODCOLIGADA> ";
+    xml += "    <IDMOV>-1</IDMOV>";
+    xml += "    <FLUIG>" + URLFluig + "</FLUIG>";
+    xml += "</TMOVCOMPL>";
+    xml += "</MovMovimento> ";
+
+    xml = xml.split("&").join("&amp;");
+    log.info("XML: " + xml);
+
+    return xml;
+}
+
+function ImportaMovimento(CODCOLIGADA, xml) {
+    var ds = DatasetFactory.getDataset("ImportaMovRM", null, [
+        DatasetFactory.createConstraint("xmlMov", xml, xml, ConstraintType.MUST),
+        DatasetFactory.createConstraint("codColigada", CODCOLIGADA, CODCOLIGADA, ConstraintType.MUST)
+    ], null);
+
+    if (!ds || ds == "" || ds == null) {
+        return [false, "Houve um erro na comunicação com o webservice. Tente novamente!"];
+    }
+    else {
+        if (ds.values[0][0] == "false") {
+            throw"Erro ao gerar movimento. Favor entrar em contato com o administrador do sistema. Mensagem: " + ds.values[0][1];
+        }
+        else if (ds.values[0][0] == "true") {
+            return ds.values[0][2];
+        }
+    }
+}
+
+function buscaLocalEstoquePorNome(CODCOLIGADA, CODFILIAL, NOME){
+    var ds = DatasetFactory.getDataset("LocalRM", null, [
+        DatasetFactory.createConstraint("clg", CODCOLIGADA, CODCOLIGADA, ConstraintType.MUST),
+        DatasetFactory.createConstraint("cdFl",CODFILIAL,CODFILIAL, ConstraintType.MUST),
+    ],null);
+
+    for (var i = 0; i < ds.values.length; i++) {
+        var nomeLocalEstoque = ds.getValue(i,"nome");
+        if (NOME == nomeLocalEstoque) {
+            return ds.getValue(i,"codloc");
+        }
+    }
+}
 function atualizaStatusTransferencia(STATUS){
     var id = hAPI.getCardValue("ID_TRANSFERENCIAS_DE_CUSTO");
 
@@ -11,4 +202,8 @@ function atualizaStatusTransferencia(STATUS){
         {type:"int", value:STATUS},
         {type:"int", value:id},
     ]);
+}
+function getServerURL(){
+    var ds = DatasetFactory.getDataset("dsGetServerURL",null,null,null);
+    return ds.getValue(0,"URL");
 }
